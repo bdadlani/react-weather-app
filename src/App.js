30da1d975384d9daac512a3d5cbbe57ea4439615
bdadlani <bdadlani17@gmail.com>
2024-11-21 23:45:14 +0530

    root

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("react-root"));
 
 function App() {
   return (
